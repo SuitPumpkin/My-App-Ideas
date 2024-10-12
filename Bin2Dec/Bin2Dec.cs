991ee79f2b4b3f8d8737d978d5862664d288de8a
Rodrigo Alejandro Loza Navarro <rodrigoalozan@gmail.com>
2024-10-11 22:40:43 -0600

    Bin2Dec

--- Bin2Dec/Bin2Dec.cs
+++ Bin2Dec/Bin2Dec.cs
@@ -2,10 +2,20 @@ using System;
 using System.Text.RegularExpressions;
 class Program
 {
-    static void Main()
+    static int Main()
     {
         Console.WriteLine("Hola! Ingresa tu número binario: ");
         string Binario = Console.ReadLine();
+        var dec = BinToDec(Binario);
+        Console.WriteLine($"El binario \"{Binario}\" en decimal es: {dec}");
+        // EXTRA
+        Console.WriteLine("Hola! Ingresa tu número decimal: ");
+        string Decimal = Console.ReadLine();
+        var bin = DecToBin(int.parse(Decimal));
+        Console.WriteLine($"El decimal \"{Decimal}\" en binario es: {bin}");
+    }
+    static int BinToDec(string Binario)
+    {
         if (Binario != null && Regex.IsMatch(Binario, "^[01]+$"))
         {
             int Decimal = 0;
@@ -15,11 +25,24 @@ class Program
                 int num = int.Parse(Binario[longitud - i - 1].ToString());
                 Decimal += num * (int)Math.Pow(2, i);
             }
-            Console.WriteLine($"El binario \"{Binario}\" en decimal es: {Decimal}");
+            return Decimal;
         }
-        else
-        {
-            Console.WriteLine("El texto ingresado no es un binario válido!!");
-        }
+        else { return 0; }
     }
+    static string DecToBin(string numeroDecimalStr)
+    {
+        if (int.TryParse(numeroDecimalStr, out int numeroDecimal))
+        {
+            if (numeroDecimal == 0) return "0";
+            string binario = "";
+            while (numeroDecimal > 0)
+            {
+                binario = (numeroDecimal % 2) + binario;
+                numeroDecimal /= 2;
+            }
+            return binario;
+        }
+        else { return ""; }
+    }
+
 }
---
+++ Bin2Dec/Descripción.md
@@ -0,0 +1,7 @@
+# Binario a Decimal
+
+Este programa tiene la función de devolver el numero decimal del binario ingresado.
+
+# Extra
+
+Tambien tiene una función para devolver un binario del numero ingresado.
